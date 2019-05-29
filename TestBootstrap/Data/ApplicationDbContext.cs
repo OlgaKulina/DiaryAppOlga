ba2d2ae4ba62c2e36c4113bc83ae5470be7e8dea
Olga Kul <helgaukrop@gmail.com>
2019-05-30 01:26:46 +0300

    Add DbSet in ApplicationDbContext

--- TestBootstrap/Data/ApplicationDbContext.cs
+++ TestBootstrap/Data/ApplicationDbContext.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TestBootstrap.Models;
 
 namespace TestBootstrap.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        DbSet<UserAim> UserAims { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            
         }
     }
 }
