2c576ee206af653e502e346d949fd5a59abf9c9c
Olga Kul <helgaukrop@gmail.com>
2019-06-05 22:21:26 +0300

    Change model UserAim

--- TestBootstrap/Models/UserAim.cs
+++ TestBootstrap/Models/UserAim.cs
@@ -7,13 +7,16 @@ namespace TestBootstrap.Models
     {
         public int Id { get; set; }
         public int UserId { get; set; }
+        
         public string Aim { get; set; }
         public string Description { get; set; }
 
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime StartDate { get; set; }
         
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EndDate { get; set;}
 
 
