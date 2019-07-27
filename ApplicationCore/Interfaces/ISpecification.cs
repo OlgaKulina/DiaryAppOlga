97f7a6237c56e5b90f99b161bd2c1366b1a880a5
Olga Kul <helgaukrop@gmail.com>
2019-07-28 00:36:13 +0300

    Changing styles pages Login&Register

--- ApplicationCore/Interfaces/ISpecification.cs
+++ ApplicationCore/Interfaces/ISpecification.cs
@@ -6,9 +6,13 @@ namespace Microsoft.DiaryAppOlga.ApplicationCore.Interfaces
 {
     public interface ISpecification<T>
     {
-
-
-
+        Expression<Func<T, bool>> Criteria { get; }
+        List<Expression<Func<T, object>>> Includes{get;}
+        List<string> IncludesStrings { get; }
+        Expression<Func<T, object>> OrderBy { get; }
+        
+        int Take { get; }
+        int Skip { get; }
 
     }
 }
