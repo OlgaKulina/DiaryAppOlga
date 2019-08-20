bdbb991450bf11bd8836733d27773a2978659e1d
Olga Kul <helgaukrop@gmail.com>
2019-08-21 02:50:48 +0300

    Added ScheduleController and Template Views for Schedules

---
+++ TestBootstrap/Controllers/ScheduleController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiaryAppOlga.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        public IActionResult AllSchedules()
+        {
+            return View();
+        }
+
+        public IActionResult Schedule()
+        {
+            return View();
+        }
+
+
+        public IActionResult MonthlyTasks()
+        {
+            return View();
+        }
+
+        public IActionResult WeeklyTasks()
+        {
+            return View();
+        }
+
+        public IActionResult DailyTasks()
+        {
+            return View();
+        }
+
+
+
+
+
+
+    }
+}
